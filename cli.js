53957a465062ab922c1f0897c00a1716584e3a0e
bagotaj <bagotaj@gmail.com>
2020-12-14 15:41:36 +0100

    Add Try/Catch to the whole code

--- cli.js
+++ cli.js
@@ -24,56 +24,52 @@ if (command._.length === 2 && Object.keys(command).length === 1) {
 let todoListContent;
 try {
     todoListContent = fs.readFileSync("todos.json");
-} catch (err) {
-    console.error(err);
-}
 
-const ToDos = new ToDoList();
+    const ToDos = new ToDoList();
 
-let toDoListFromFile = JSON.parse(todoListContent);
-toDoListFromFile.forEach((element) => {
-    ToDos.add(element.content, element.status);
-});
+    let toDoListFromFile = JSON.parse(todoListContent);
+    toDoListFromFile.forEach((element) => {
+        ToDos.add(element.content, element.status);
+    });
 
-if (command.l === true) {
-    if (ToDos.getLength() === 0) {
-        errorHandling("noToDo");
+    if (command.l === true) {
+        if (ToDos.getLength() === 0) {
+            errorHandling("noToDo");
+        }
+
+        console.log(ToDos.toString());
     }
 
-    console.log(ToDos.toString());
-}
-
-if (typeof command.a === "string") {
-    ToDos.add(command.a);
-} else if (command.a === true) {
-    errorHandling("commandA");
-}
-
-if (typeof command.r === "number") {
-    if (ToDos.getLength() < command.r) {
-        errorHandling("commandROverIndexing");
-    } else {
-        ToDos.delete(command.r);
+    if (typeof command.a === "string") {
+        ToDos.add(command.a);
+    } else if (command.a === true) {
+        errorHandling("commandA");
     }
-} else if (command.r === true) {
-    errorHandling("commandR");
-} else if (typeof command.r === "string") {
-    errorHandling("commandRNotANumber");
-}
 
-if (typeof command.c === "number") {
-    if (ToDos.getLength() < command.c) {
-        errorHandling("commandCOverIndexing");
-    } else {
-        ToDos.complete(command.c);
+    if (typeof command.r === "number") {
+        if (ToDos.getLength() < command.r) {
+            errorHandling("commandROverIndexing");
+        } else {
+            ToDos.delete(command.r);
+        }
+    } else if (command.r === true) {
+        errorHandling("commandR");
+    } else if (typeof command.r === "string") {
+        errorHandling("commandRNotANumber");
     }
-} else if (command.c === true) {
-    errorHandling("commandC");
-} else if (typeof command.c === "string") {
-    errorHandling("commandCNotANumber");
-}
 
-try {
+    if (typeof command.c === "number") {
+        if (ToDos.getLength() < command.c) {
+            errorHandling("commandCOverIndexing");
+        } else {
+            ToDos.complete(command.c);
+        }
+    } else if (command.c === true) {
+        errorHandling("commandC");
+    } else if (typeof command.c === "string") {
+        errorHandling("commandCNotANumber");
+    }
+
     todoListContent = JSON.stringify(ToDos.getToDoList());
     fs.writeFileSync("todos.json", todoListContent);
 } catch (err) {
